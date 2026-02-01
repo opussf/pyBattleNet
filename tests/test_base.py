7aa272afb6c8fbcd3415184905ba82f048064aea
opussf <github@zz9-za.com>
2026-02-01 03:14:29 -0800

    Tests with mocking are working.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,6 @@
 import pytest
 from unittest.mock import patch, mock_open, Mock
+import urllib
 
 import pybattlenet
 
@@ -17,7 +18,7 @@ class TestBuiltInFunctions:
 		mock_exists.return_value = False
 
 		with pytest.raises(SystemExit) as exc_info:
-			BN = pybattlenet.PyBattleNet(region="us")
+			pybattlenet.PyBattleNet(region="us")
 
 		# Verify it exited with code 1
 		assert exc_info.value.code == 1
@@ -30,7 +31,7 @@ class TestBuiltInFunctions:
 		mock_exists.return_value = False
 
 		with pytest.raises(SystemExit) as exc_info:
-			BN = pybattlenet.PyBattleNet(region="us", clientID="Frank")
+			pybattlenet.PyBattleNet(region="us", clientID="Frank")
 		assert exc_info.value.code == 1
 
 	@patch('os.path.exists')
@@ -41,14 +42,14 @@ class TestBuiltInFunctions:
 		mock_exists.return_value = False
 
 		with pytest.raises(SystemExit) as exc_info:
-			BN = pybattlenet.PyBattleNet(region="us", secret="Frank")
+			pybattlenet.PyBattleNet(region="us", secret="Frank")
 		assert exc_info.value.code == 1
 
 	@patch('urllib.request.urlopen')
 	@patch('builtins.open', new_callable=mock_open, read_data='{"CLIENTID": "Frank", "BLSECRET": "Shhhh"}')
 	@patch('os.path.exists')
 	@patch('os.path.expanduser')
-	def test_success_sets_access_token(self, mock_expanduser, mock_exists, mock_file, mock_urlopen):
+	def test_success_sets_access_token_fromFile(self, mock_expanduser, mock_exists, mock_file, mock_urlopen):
 		""" Test that a missing secrets file exits the object """
 		# setup mocks
 		mock_expanduser.return_value = '/home/testuser/.file.json'
@@ -58,12 +59,112 @@ class TestBuiltInFunctions:
 		mock_response.read.return_value = b'{"access_token": "t0k3n"}'
 		mock_response.status = 200  # This should work
 
-		# Set up the context manager
-		mock_urlopen.return_value.__enter__.return_value = mock_response
-		mock_urlopen.return_value.__exit__.return_value = None
+		mock_urlopen.return_value = mock_response
 
 		BN = pybattlenet.PyBattleNet(region="us")
 		assert BN.access_token == "t0k3n"
+
+	@patch('urllib.request.urlopen')
+	@patch('builtins.open', new_callable=mock_open, read_data='')
+	@patch('os.path.exists')
+	@patch('os.path.expanduser')
+	def test_success_sets_access_token_passed(self, mock_expanduser, mock_exists, mock_file, mock_urlopen):
+		""" Test that a missing secrets file exits the object """
+		# setup mocks
+		mock_expanduser.return_value = '/home/testuser/.file.json'
+		mock_exists.return_value = False
+
+		mock_response = Mock()
+		mock_response.read.return_value = b'{"access_token": "t0k3n"}'
+		mock_response.status = 200  # This should work
+
+		mock_urlopen.return_value = mock_response
+
+		BN = pybattlenet.PyBattleNet(region="us", clientID="Frank", secret="Shhh")
+		assert BN.access_token == "t0k3n"
+
+	@patch('urllib.request.urlopen')
+	@patch('builtins.open', new_callable=mock_open, read_data='')
+	@patch('os.path.exists')
+	@patch('os.path.expanduser')
+	def test_odd_return_status(self, mock_expanduser, mock_exists, mock_file, mock_urlopen):
+		""" Test that a missing secrets file exits the object """
+		# setup mocks
+		mock_expanduser.return_value = '/home/testuser/.file.json'
+		mock_exists.return_value = False
+
+		mock_response = Mock()
+		mock_response.read.return_value = b'{"access_token": "t0k3n"}'
+		mock_response.status = 207  # This should work
+
+		mock_urlopen.return_value = mock_response
+
+		with pytest.raises(SystemExit) as excinfo:
+			pybattlenet.PyBattleNet(region="us", clientID="Frank", secret="Shhh")
+
+		assert excinfo.value.code == 207
+
+	@patch('urllib.request.urlopen')
+	@patch('builtins.open', new_callable=mock_open, read_data='')
+	@patch('os.path.exists')
+	@patch('os.path.expanduser')
+	def test_httperror_404_token_passed(self, mock_expanduser, mock_exists, mock_file, mock_urlopen):
+		""" Test that a missing secrets file exits the object """
+		# setup mocks
+		mock_expanduser.return_value = '/home/testuser/.file.json'
+		mock_exists.return_value = False
+
+		# create an HTTPError
+		http_error = urllib.error.HTTPError(
+			url='https://example.com/token',
+			code=401,
+			msg='Unauthorized',
+			hdrs=None,
+			fp=None,
+		)
+		mock_urlopen.side_effect = http_error
+
+		with pytest.raises(SystemExit) as excinfo:
+			pybattlenet.PyBattleNet(region="us", clientID="Frank", secret="Shhh")
+
+		assert excinfo.value.code == 1
+
+	@patch('urllib.request.urlopen')
+	@patch('builtins.open', new_callable=mock_open, read_data='')
+	@patch('os.path.exists')
+	@patch('os.path.expanduser')
+	def test_urlerror_token_passed(self, mock_expanduser, mock_exists, mock_file, mock_urlopen):
+		""" Test that a missing secrets file exits the object """
+		# setup mocks
+		mock_expanduser.return_value = '/home/testuser/.file.json'
+		mock_exists.return_value = False
+
+		# create an URLError
+		url_error = urllib.error.URLError("Connection refused")
+		mock_urlopen.side_effect = url_error
+
+		with pytest.raises(SystemExit) as excinfo:
+			pybattlenet.PyBattleNet(region="us", clientID="Frank", secret="Shhh")
+
+		assert excinfo.value.code == 1
+
+	@patch('urllib.request.urlopen')
+	@patch('builtins.open', new_callable=mock_open, read_data='')
+	@patch('os.path.exists')
+	@patch('os.path.expanduser')
+	def test_unknown_exception(self, mock_expanduser, mock_exists, mock_file, mock_urlopen):
+		""" Test that a missing secrets file exits the object """
+		# setup mocks
+		mock_expanduser.return_value = '/home/testuser/.file.json'
+		mock_exists.return_value = False
+
+		mock_urlopen.side_effect = Exception("Boom")
+
+		with pytest.raises(SystemExit) as excinfo:
+			pybattlenet.PyBattleNet(region="us", clientID="Frank", secret="Shhh")
+
+		assert excinfo.value.code == 1
+
 
 
 
