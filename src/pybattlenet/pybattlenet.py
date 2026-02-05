9f0ce15c43d5e61285d066c0e4d592bb4fce1500
opussf <github@zz9-za.com>
2026-02-05 02:16:16 -0800

    Central request call

--- src/pybattlenet/pybattlenet.py
+++ src/pybattlenet/pybattlenet.py
@@ -18,59 +18,48 @@ class PyBattleNet():
 	def __init__(self, region: str, logger: logging.Logger | None = None,
 				clientID: str | None = None, secret: str | None = None) -> None:
 		self.logger = logger
-		self.__set_secrets(clientID, secret)
-
-		# get the access token
 		self.region = region
-		self.request = urllib.request.Request("https://oauth.battle.net/token")
-		userpassword = base64.b64encode((f'{self.clientID}:{self.secret}').encode('ascii'))
-		self.request.add_header("Authorization", "Basic %s" % userpassword.decode('ascii'))
-		self.context = ssl._create_unverified_context()
-		self.request.add_header("User-Agent",
-				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36')
-		data = urllib.parse.urlencode({'grant_type': 'client_credentials'}).encode('utf-8')
+		self.__set_secrets(clientID, secret)
+		self.__get_access_token()
+
+	def __make_request(self, data: bytes | None = None):
 		try:
 			result = urllib.request.urlopen(self.request, context=self.context, data=data)
 			if result.status != 200:
 				self.__logError(f"Unexpected status code: {result.status}")
-				sys.exit(result.status)
+				raise RuntimeError(f"HTTP request failed with status code {result.status}")
 			else:
-				tokenJSON = result.read().decode('utf-8')
-				self.access_token = json.loads(tokenJSON)['access_token']
+				return result
 		except urllib.error.HTTPError as e:
 			# This handles HTTP status codes like 404, 500, etc.
 			self.__logError(f"HTTP error: {e.code} - {e.reason}")
-			sys.exit(1)
+			raise(e)
 		except urllib.error.URLError as e:
 			# This handles connection errors, DNS errors, etc.
 			self.__logError(f"URL error: {e.reason}")
-			sys.exit(1)
+			raise(e)
 		except Exception as e:
 			# Any other unexpected errors
 			self.__logError(f"Unexpected error: {e}")
-			sys.exit(1)
+			raise e
+
+	def __get_access_token(self):
+		self.request = urllib.request.Request("https://oauth.battle.net/token")
+		userpassword = base64.b64encode((f'{self.clientID}:{self.secret}').encode('ascii'))
+		self.request.add_header("Authorization", "Basic %s" % (userpassword.decode('ascii'),))
+		self.context = ssl._create_unverified_context()
+		self.request.add_header("User-Agent",
+				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36')
+		data = urllib.parse.urlencode({'grant_type': 'client_credentials'}).encode('utf-8')
+		tokenJSON = self.__make_request(data).read().decode('utf-8')
+		self.access_token = json.loads(tokenJSON)['access_token']
 
 	def __makeAPIRequest(self, endPoint: str):
 		""" This sets self.request """
 		url = f'https://{self.region}.api.blizzard.com{endPoint}'
 		self.request = urllib.request.Request(url)
 		self.request.add_header("Authorization", "Bearer %s" % self.access_token)
-		try:
-			result = urllib.request.urlopen(self.request, context=self.context)
-			if result.status != 200:
-				self.__logError(f"Unexpected status code: {result.status}")
-				sys.exit(result.status)
-			else:
-				return result
-		except urllib.error.HTTPError as e:
-			# This handles HTTP status codes like 404, 500, etc.
-			self.__logError(f"HTTP error: {e.code} - {e.reason}")
-		except urllib.error.URLError as e:
-			# This handles connection errors, DNS errors, etc.
-			self.__logError(f"URL error: {e.reason}")
-		except Exception as e:
-			# Any other unexpected errors
-			self.__logError(f"Unexpected error: {e}")
+		return self.__make_request()
 
 	def __set_secrets(self, clientID: str | None = None, secret: str | None = None):
 		""" This sets the required secrets needed to access bnet API services.
